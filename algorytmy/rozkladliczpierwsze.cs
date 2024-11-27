b235730f3e385756ef11ccc67bcece6f78ada8dc
admin <admin@ZMIEN-MNIE>
2024-11-27 14:13:55 +0100

    gotowe

--- algorytmy/rozkladliczpierwsze.cs
+++ algorytmy/rozkladliczpierwsze.cs
@@ -10,6 +10,30 @@ namespace algorytmy
     {
         public static void Run()
         {
+            Console.WriteLine("Podaj liczbę do rozkładu na czynniki pierwsze : ");
+
+            int liczba = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Czynniki pierwsze: ");
+            Factorize(liczba);
+
+        }
+
+        private static void Factorize(int liczba)
+        {
+            for (int i = 2; i <= Math.Sqrt(liczba); i++)
+            {
+                while (liczba % i == 0)
+                {
+                    Console.WriteLine(i);
+                    liczba /= i;
+                }
+            }
+            if (liczba > 1)
+            {
+                Console.WriteLine(liczba);
+            }
+
         }
     }
 }
