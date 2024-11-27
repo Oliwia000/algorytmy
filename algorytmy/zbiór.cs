c5f496e4d6f865a290107a5233a956a53cd7dcc7
admin <admin@ZMIEN-MNIE>
2024-11-27 14:54:37 +0100

    .

--- algorytmy/zbiór.cs
+++ algorytmy/zbiór.cs
@@ -10,6 +10,30 @@ namespace algorytmy
     {
         public static void Run()
         {
+            Console.WriteLine("Podaj liczby oddzielone spacjami:");
+
+            string[] input = Console.ReadLine().Split(' ');
+            int[] liczby = Array.ConvertAll(input, int.Parse);
+
+            int min = FindMin(liczby);
+            int max = FindMax(liczby);
+
+            Console.WriteLine($"Najmniejszy element: {min}");
+            Console.WriteLine($"Największy element:  {max}");
+
         }
+        private static int FindMin(int[] liczby)
+        {
+            int min = liczby[0];
+
+        }
+
+
+
+        private static int FindMax(int[] liczby) 
+        { 
+        }
+
     }
 }
+
