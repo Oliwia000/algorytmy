51953d20ecc3131dcf1ac108e1a3c140301399bd
teren <teren@KOMP>
2024-11-27 20:17:31 +0100

    .

--- algorytmy/bąbelkowe.cs
+++ algorytmy/bąbelkowe.cs
@@ -8,16 +8,21 @@ namespace algorytmy
 {
     public static class bąbelkowe
     {
+
+        /// Główna funkcja uruchamiająca algorytm sortowania bąbelkowego.
+        /// Użytkownik podaje liczby, które mają zostać posortowane.
+
+
         public static void Run()
         {
             Console.WriteLine("Podaj liczby do posortowania oddzielone spacjami:");
-            string[] input = Console.ReadLine().Split(' ');
+            string[] input = Console.ReadLine().Split(' ');     // Wczytywanie danych
             int[] liczby = Array.ConvertAll(input, int.Parse);
 
             Sort(liczby);
 
             Console.WriteLine("Posortowane liczby:");
-            Console.WriteLine(string.Join(" ", liczby));
+            Console.WriteLine(string.Join(" ", liczby));   // Wyświetlenie posortowanych liczb
         }
 
         private static void Sort(int[] liczby)
@@ -29,6 +34,7 @@ namespace algorytmy
                 {
                     if (liczby[j] > liczby[j + 1])
                     {
+                        // Wymiana miejscami elementów
                         int temp = liczby[j];
                         liczby[j] = liczby[j + 1];
                         liczby[j + 1] = temp;
