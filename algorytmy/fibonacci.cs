0ea05c664d110bd95363f193c2529e0859311167
teren <teren@KOMP>
2024-12-01 17:36:16 +0100

    .

--- algorytmy/fibonacci.cs
+++ algorytmy/fibonacci.cs
@@ -11,19 +11,21 @@ namespace algorytmy
         public static void Run()
         {
 
-            Console.WriteLine("Podaj liczbę n (elem.cięgu Fibonacciego):");
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 0; i <= n; i++)
+            Console.WriteLine("Podaj liczbę n (elem.cięgu Fibonacciego):");  // Odczytanie liczby n, czyli liczby elementów ciągu Fibonacciego, do których chcemy obliczyć wartości.
+            int n = int.Parse(Console.ReadLine());    // Pętla, która iteruje od 0 do n, wyświetlając dla każdego indeksu i wartość F(i) ciągu Fibonacciego.
+            for (int i = 0; i <= n; i++)  
             {
+                // Wyświetlenie obliczonej wartości F(i)
                 Console.WriteLine($"F({i})={CalculateFibonacci(i)}");
             }
         }
 
         public static int CalculateFibonacci(int n)
         {
+            // Warunek zakończenia : jeśli n jest mniejsze lub równe 1, zwróć wartość n.
             if (n <= 1)
-                return n; 
-
+                return n;
+            //Wywołanie dla dwóch poprzednich elementów: F(n-1) i F(n-2), a następnie zwrócenie ich sumy.
             return CalculateFibonacci(n - 1) + CalculateFibonacci(n - 2); 
         }
     }
