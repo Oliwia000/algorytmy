be03e0e4d1576a698163780480dc8059fb228d9c
teren <teren@KOMP>
2024-12-01 17:44:54 +0100

    .

--- algorytmy/rozkladliczpierwsze.cs
+++ algorytmy/rozkladliczpierwsze.cs
@@ -12,20 +12,22 @@ namespace algorytmy
         {
             Console.WriteLine("Podaj liczbę do rozkładu na czynniki pierwsze : ");
 
+            // Odczytanie danych wejściowych i zminna na liczbę całkowitą.
             int liczba = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Czynniki pierwsze: ");
-            Factorize(liczba);
+            Console.WriteLine("Czynniki pierwsze: ");  // Wyświetlenie nagłówka wyników.
+            Factorize(liczba);   // Wywołanie metody Factorize, która rozkłada liczbę na czynniki pierwsze.
 
         }
 
         private static void Factorize(int liczba)
         {
+            // Pętla iterująca przez możliwe dzielniki od 2 do pierwiastka z liczby.
             for (int i = 2; i <= Math.Sqrt(liczba); i++)
             {
-                while (liczba % i == 0)
+                while (liczba % i == 0)  // Sprawdzenie, czy liczba jest podzielna przez i
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine(i);   // Wypisanie czynnika pierwszego i podzielenie liczby przez i.
                     liczba /= i;
                 }
             }
